61013a5318b5e137fae5ada32d2a6ca06a2a982e
Fersandi Pratama <fersandi_pratama@rocketmail.com>
2018-03-23 16:47:54 +0700

    Edit bisection implemention

--- bisection/bisection.m
+++ bisection/bisection.m
@@ -1,10 +1,14 @@
 function c = bisection(fun, a, b, tol)
    NMAX = 10000;
    
+   if sign(fun(a)) == sign(fun(b))
+    error("Same sign, no solution")
+   endif
+   
    n =  1;
    while n < NMAX
     c = (a + b)/2;
-     if abs(fun(c)) < tol
+     if abs(fun(c)) == 0 || (b-a)/2 < tol
       return
      endif
    
--- bisection/test.m
+++ bisection/test.m
@@ -3,5 +3,3 @@ soal2 = @(x) x^x - 2
 soal3 = @(x) 10 * e^(-x) * sin(2 * pi * x) - 2
 soal4 = @(x) x^4 - 6 * x^3 + 12 * x^2 - 10 * x + 3
 soal5 = @(x) x^2 - cos(pi * x)
-
- 
